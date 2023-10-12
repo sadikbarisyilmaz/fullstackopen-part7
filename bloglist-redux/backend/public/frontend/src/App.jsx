ebe02b4d9608953461f1d0bc2af209b4a5bbde50
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-10-12 16:22:22 +0300

    path fix

--- bloglist-redux/backend/public/frontend/src/App.jsx
+++ bloglist-redux/backend/public/frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { Home } from "./components/Home";
 import { Users } from "./components/Users";
 import { Nav } from "./components/Nav";
 import { Footer } from "./components/Footer";
-import { User } from "./components/User.JSX";
+import { User } from "./components/User.jsx";
 import { loginUser } from "./reducers/userReducer";
 import { Blog } from "./components/Blog";
 import { Notification } from "./components/Notification";
