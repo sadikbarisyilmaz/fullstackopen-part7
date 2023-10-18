a67f4b6126fb8abf0cfbe01a33ce848043269f5b
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-10-19 00:54:34 +0300

    styling

--- bloglist-redux/backend/public/frontend/src/components/Blogs.jsx
+++ bloglist-redux/backend/public/frontend/src/components/Blogs.jsx
@@ -12,26 +12,29 @@ export const Blogs = ({ handleLike, showNotification, blogs, loggedUser }) => {
 
   return (
     <div className="w-full pt-[70px] animate-fadeIn flex bg-[#fffdfa] flex-col justify-center grow">
-      <div className="pt-20 md:px-44 px-6 pb-16 flex flex-col text-center py-4 justify-center items-center gap-8 md:gap-14">
-        <h2 className="text-3xl md:text-7xl text-[#ff5a19]">
-          Discover Awesome Blogs !
-        </h2>
-        <p className="max-w-xl ">
-          Explore a world of interesting blogs, where users share their favorite
-          reads. Whether you're into travel, food, or thought-provoking essays,
-          our platform has a variety of content that fellow users have enjoyed.
-          Join the conversation, discover new things, and connect with
-          like-minded folks by commenting on your favorite blogs !
-        </p>
-        <span className="flex w-fit rounded-md gap-2">
-          <input
-            placeholder="Search Blogs"
-            onChange={(e) => setFilter(e.target.value)}
-            value={filter}
-            className="input p-2"
-            type="text"
-          />
-        </span>
+      <div className="bg-[url('.././public/bg-home.jpg')] bg-no-repeat bg-cover ">
+        <div className="flex flex-col text-center w-full justify-center items-center pt-20 md:px-44 px-6 pb-16 py-4  md:gap-14 bg-black bg-opacity-60  gap-8 backdrop-blur-sm">
+          <h2 className="text-3xl md:text-7xl drop-shadow-2xl text-[#ff5a19] font-bold">
+            Discover Awesome Blogs !
+          </h2>
+          <p className="max-w-xl text-white drop-shadow-2xl  font-bold md:text-base text-xs">
+            Explore a world of interesting blogs, where users share their
+            favorite reads. Whether you're into travel, food, or
+            thought-provoking essays, our platform has a variety of content that
+            fellow users have enjoyed. Join the conversation, discover new
+            things, and connect with like-minded folks by commenting on your
+            favorite blogs !
+          </p>
+          <span className="flex w-fit rounded-md gap-2">
+            <input
+              placeholder="Search Blogs"
+              onChange={(e) => setFilter(e.target.value)}
+              value={filter}
+              className="input p-2"
+              type="text"
+            />
+          </span>
+        </div>
       </div>
       <div className="md:px-36 px-6 py-12 ">
         {blogs.length > 0 ? (
