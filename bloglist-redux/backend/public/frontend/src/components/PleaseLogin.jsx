63857fdd66a3289a7ed5f71e2d0f626f9a20c62c
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-10-19 15:50:04 +0300

    styling

--- bloglist-redux/backend/public/frontend/src/components/PleaseLogin.jsx
+++ bloglist-redux/backend/public/frontend/src/components/PleaseLogin.jsx
@@ -5,7 +5,7 @@ export const PleaseLogin = () => {
     <div className="animate-fadeIn w-full flex grow bg-[url('.././public/bg-home.jpg')] bg-no-repeat bg-cover items-center justify-center gap-2 flex-col ">
       <div className="p-6 flex items-center justify-center backdrop-blur-sm w-full grow">
         <div className="bg-white flex gap-3 flex-col p-8 md:p-24 rounded-md justify-center items-center">
-          <h1 className="text-[#ff5a19] font-semibold text-3xl md:text-3xl py-2 drop-shadow-lg">
+          <h1 className="text-[#ff5a19] font-semibold text-center text-xl md:text-3xl py-2 drop-shadow-lg">
             Please Login to Continue
           </h1>
 
